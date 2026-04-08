c177df72af01176fdfe5dc989f4fe7f49b1abedc
Siddharth <siddharth.malviya@sprinklr.com>
2026-04-08 07:57:10 +0530

    added suppress warning

--- src/ArrayPrograms/ArrayProblemsAdvance.java
+++ src/ArrayPrograms/ArrayProblemsAdvance.java
@@ -5,6 +5,7 @@ import java.util.*;
 
 import static ArrayPrograms.ArrayProblems.reverse;
 
+@SuppressWarnings("all")
 public class ArrayProblemsAdvance {
 
     public static void main(String[] args) {
